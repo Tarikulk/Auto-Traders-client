651b2218b5b01e3bec658b556e1a599aa535c3b2
Tarikulk <ullahtarik300@gmail.com>
2022-11-24 17:16:04 +0600

    authentication added

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,19 +1,43 @@
 import React, { createContext, useState } from 'react';
 import app from '../Firebase/Firebase.config';
-import {createUserWithEmailAndPassword, getAuth} from "firebase/auth"
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, updateProfile} from "firebase/auth"
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null)
+ 
 
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const authInfo = {user, createUser}
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+      }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const resetPassword = email => {
+        return sendPasswordResetEmail(auth, email)
+    }
+    
+
+    
+  //   2. Update Name
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    })
+  }
+
+    const authInfo = {user, createUser, updateUserProfile, signIn, resetPassword, signInWithGoogle}
  
     return (
         <div>
--- src/Pages/Authentication/SignUp.js
+++ src/Pages/Authentication/SignUp.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react' 
+import toast from 'react-hot-toast';
 import { Link, useLocation, useNavigate } from 'react-router-dom' 
 import { AuthContext } from '../../Contexts/AuthProvider';
 
 const Signup = () => {
 
-  const {createUser, updateUserProfile, verifyEmail, loading, setLoading, signInWithGoogle} = useContext(AuthContext);
+  const {createUser, updateUserProfile, loading, setLoading, signInWithGoogle} = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/"
@@ -32,11 +33,10 @@ const Signup = () => {
       createUser(email, password)
       .then(result =>{
         const user = result.user;  
+        console.log(user)
         updateUserProfile(name, data.data.display_url)
         .then(
-          verifyEmail().then(() =>{
-            navigate(from, {replace: true})
-          })
+          toast.success("user create successfully")
         )
         .catch(error => console.error(error))
         form.reset();
@@ -45,9 +45,20 @@ const Signup = () => {
 
     })
     .catch(error =>  {
-      setLoading(false)
+      toast.error(error.message)
     }) 
   }  
+
+
+  const handleGoogleSignIn = () =>{
+    signInWithGoogle()
+    .then(result => {
+      const user = result.user;
+      toast.success("Login successful")
+      console.log(user)
+    })
+    .catch(error => console.error(error))
+  }
  
 
   return (
@@ -74,7 +85,7 @@ const Signup = () => {
                 id='name'
                 required
                 placeholder='Enter Your Name Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-700 bg-gray-200 text-gray-900'
                 data-temp-mail-org='0'
               />
             </div>
@@ -100,7 +111,7 @@ const Signup = () => {
                 name='email'
                 id='email'
                 placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-700 bg-gray-200 text-gray-900'
                 data-temp-mail-org='0'
               />
             </div>
@@ -116,10 +127,32 @@ const Signup = () => {
                 id='password'
                 required
                 placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-200 focus:outline-green-500 text-gray-900'
+                className='w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-200 focus:outline-indigo-700 text-gray-900'
               />
             </div>
           </div>
+
+          <div className='flex items-center pt-4 space-x-1'>
+          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+          <p className='px-3 text-sm dark:text-gray-400'>
+            Select Role
+          </p>
+          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+        </div>
+
+                <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Buyer</span> 
+            <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Seller</span> 
+            <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked />
+          </label>
+        </div>
+
           <div className='space-y-2'>
           <div className='w-full text-center'>
              <button type="submit" className='hover:text-gray-100 text-white bg-indigo-700 px-10 py-2 rounded-lg'>Sign In</button>
@@ -134,7 +167,7 @@ const Signup = () => {
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
         <div className='flex justify-center space-x-4'>
-          <button aria-label='Log in with Google' className='p-3 rounded-sm'>
+          <button onClick={handleGoogleSignIn} aria-label='Log in with Google' className='p-3 rounded-sm'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 32 32'
@@ -143,7 +176,7 @@ const Signup = () => {
               <path d='M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z'></path>
             </svg>
           </button>
-          <button aria-label='Log in with Twitter' className='p-3 rounded-sm'>
+          <button aria-label='Log in with Twitter' className='p-3 rounded-sm btn-disabled'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 32 32'
@@ -152,7 +185,7 @@ const Signup = () => {
               <path d='M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z'></path>
             </svg>
           </button>
-          <button aria-label='Log in with GitHub' className='p-3 rounded-sm'>
+          <button aria-label='Log in with GitHub' className='p-3 rounded-sm btn-disabled'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 32 32'
@@ -164,7 +197,7 @@ const Signup = () => {
         </div>
         <p className='px-6 text-sm text-center text-gray-400'>
           Already have an account yet?{' '}
-          <Link to='/login' className='hover:underline text-gray-600'>
+          <Link to='/signIn' className='hover:underline text-gray-600'>
             Sign In
           </Link>
           .
